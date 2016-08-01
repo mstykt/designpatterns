3ec62e74e50de9738eb012c89dd07483f9963cb2
Mesut Yakut <mst@mesutyakut.com>
2016-08-01 08:17:25 +0300

    Factory Design Pattern package-info class has been added.

---
+++ src/main/java/com/designpatterns/factory/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Created by mesut on 01.08.2016.
+ * <br />
+ * This package is example of Factory Design Pattern
+ * Factory Design pattern does not %100 build on SOLID principle
+ * There is a Open Closed Principle exception
+ */
+package com.designpatterns.factory;
